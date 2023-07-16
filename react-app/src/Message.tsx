347cef0cb5075f698822e9a698fc025635fe049e
Joe McCuen <joemccuen3@gmail.com>
2023-07-16 18:25:55 -0400

    2nd commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+// PascalCasing is used for React Apps,
+// instead of camel casing
+function Message() {
+    // JSX -> JavaScript XML
+    const name = 'Joe'
+    return <h1>Hello {name}</h1>
+}
+
+export default Message
